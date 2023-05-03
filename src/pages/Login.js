4e6d8d62602b6f3065521bc4556e2343a1dd747f
Octavio Martinez <omartinez1618@gmail.com>
2023-05-03 08:49:59 -0400

    feat(Login.js): add avatar, email and password text fields and style login card

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,5 @@
 import { useLogin } from 'react-facebook'
-import { Box, Card, Button } from '@mui/material'
+import { Box, Card, Button, TextField, Avatar } from '@mui/material'
 
 export default function Login () {
     const { login, status, isLoading, error} = useLogin();
@@ -16,11 +16,36 @@ export default function Login () {
       }
     }
 
-    return <Box>
-        <Card>
+    return <Box sx={styles.container}>
+        <Card sx={styles.card}>
+            <Avatar />
+            <TextField variant='outlined' />
+            <TextField variant='outlined' />
+            <Button variant='outlined'>Login</Button>
             <Button variant='contained' onClick={handleLogin} disabled={isLoading}>
                 Login via Facebook
             </Button>
         </Card>
     </Box>
+}
+
+const styles = {
+    container: {
+        display:'flex',
+        justifyContent:'center',
+        alignItems:'center',
+        height:'100vh',
+    },
+    card:{
+        display:'flex',
+        flexDirection:'column',
+        boxShadow:'0 0 1.5rem #0005',
+        p:5,
+        gap:2,
+        '& .MuiAvatar-root':{
+            alignSelf:'center',
+            height:'8rem',
+            width:'8rem',
+        }
+    }
 }
